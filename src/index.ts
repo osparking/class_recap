af87e824b32abeb33b25d8bab87d04c0f377f69b
BumTiger <jbpark03@gmail.com>
2025-09-17 16:37:20 +0900

    param properties shortcut notation

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
 class Player {
-  public readonly last: string;
-  public readonly first: string;
   private score = 0;
-  constructor(last: string, first: string) {
-    this.last = last;
-    this.first = first;
-  }
+  constructor(public readonly last: string, public readonly first: string) {}
 
-  public callMe() {    
+  public callMe() {
     this.methodA();
   }
   private methodA() {
-    console.log('이것은 1급 비밀이다.')
+    console.log("이것은 1급 비밀이다.");
   }
 }
 
 const singer = new Player("갑자기 보다", "턴넬");
-singer.methodA();
 singer.callMe();
