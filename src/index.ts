066438965d470090fa2d20c97143a835804ea851
BumTiger <jbpark03@gmail.com>
2025-09-17 15:43:52 +0900

    public is used for clarity

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 class Player {
-  readonly last: string;
-  readonly first: string;
-  score = 0;
+  public readonly last: string;
+  public readonly first: string;
+  public score = 0;
   constructor(last: string, first: string) {
     this.last = last;
     this.first = first;
@@ -10,4 +10,3 @@ class Player {
 
 const singer = new Player("갑자기 보다", "턴넬");
 singer.score = 99;
-singer.first = '지영';
