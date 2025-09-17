ba200bc222c97073a280b7c9f44caf2fd1aa5ac6
BumTiger <jbpark03@gmail.com>
2025-09-17 16:07:08 +0900

    private field and method called in and out

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 class Player {
   public readonly last: string;
   public readonly first: string;
-  public score = 0;
+  private score = 0;
   constructor(last: string, first: string) {
     this.last = last;
     this.first = first;
   }
+
+  public callMe() {    
+    this.methodA();
+  }
+  private methodA() {
+    console.log('이것은 1급 비밀이다.')
+  }
 }
 
 const singer = new Player("갑자기 보다", "턴넬");
-singer.score = 99;
+singer.methodA();
+singer.callMe();
