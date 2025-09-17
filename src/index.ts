9890f2db87ab13780c674abd2b42985628684d50
BumTiger <jbpark03@gmail.com>
2025-09-17 17:16:22 +0900

    getter and setter for _score field

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ class Player {
   constructor(
     public readonly last: string,
     public readonly first: string,
-    private score: number
+    private _score: number
   ) {}
 
   public callMe() {
@@ -12,10 +12,23 @@ class Player {
     console.log("이것은 1급 비밀이다.");
   }
 
-  get fullName() {
+  get fullName(): string {
     return `${this.last}${this.first}`;
+  }
+
+  get score(): number {
+    return this._score;
+  }
+
+  set score(newScore: number) {
+    if (newScore < 0) {
+      throw new Error("음수 성적은 불가능합니다.");
+    } else {
+      this._score = this.score;
+    }
   }
 }
 
 const singer = new Player("제갈", "공명", 0);
 console.log(singer.fullName);
+singer.score = -1;
