d9b21efed59887cbfa628809898c717d5c852343
BumTiger <jbpark03@gmail.com>
2025-09-18 16:48:17 +0900

    generic function that returns random element of give type

--- src/generic.ts
+++ src/generic.ts
@@ -15,7 +15,7 @@ function stringIdentity(id: string): string {
 // }
 
 // 헤법: generic function
-function identity<T>(id: T) : T {
+function identity<T>(id: T): T {
   return id;
 }
 
@@ -24,9 +24,18 @@ console.log(`주문번호 : ${identity<number>(12345)}`);
 console.log(`핫 소스 추가 : ${identity<boolean>(true)}`);
 
 type Cat = {
-  name: string,
-  age: number
-}
+  name: string;
+  age: number;
+};
 
-const myCat = identity<Cat>({name: "나비", age: 16});
+const myCat = identity<Cat>({ name: "나비", age: 16 });
 console.log(myCat);
+
+function getRandomElement<T>(elements: T[]): T | undefined {
+  if (elements.length === 0) {
+    return undefined;
+  } else {
+    const idx = Math.floor(Math.random() * elements.length);
+    return elements[idx];
+  }
+}
