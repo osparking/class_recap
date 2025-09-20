42607a7ba1be72347bea9ddef172bea76dbe130e
BumTiger <jbpark03@gmail.com>
2025-09-20 09:54:20 +0900

    in JS, type of word is any everywhere

--- src/narrow.ts
+++ src/narrow.ts
@@ -1,4 +1,4 @@
-function triple (argu : number | string) {
+function triple(argu: number | string) {
   if (typeof argu === "number") {
     return argu * 3;
   } else {
@@ -8,3 +8,11 @@ function triple (argu : number | string) {
 
 console.log(triple(100));
 console.log(triple("신기하다 "));
+
+const printLetters = (word: string | null) => {
+  if (word) {
+    word.split("").forEach((letter) => console.log(letter));
+  } else {
+    console.log(`제공된 인자${word}는 스트링이 아니다.`);
+  }
+};
