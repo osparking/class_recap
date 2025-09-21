e84ad44728e2a0524fe57bcd9002d9f2c241b677
BumTiger <jbpark03@gmail.com>
2025-09-21 13:04:38 +0900

    type predicate usage - syntax, effect

--- src/narrow.ts
+++ src/narrow.ts
@@ -8,13 +8,14 @@ class DogType {
 
 const makeSound = (pet : CatType | DogType) => {
   if (isCat(pet)) {
-    return `야옹. 엄마 있다:`;
+    return `야옹. 엄마 있다: ${pet.hasMom}`;
   } else {
+    pet
     return "멍멍";
   }
 }
 
-function isCat(pet : CatType | DogType){
+function isCat(pet : CatType | DogType) :pet is CatType {
   return (pet as CatType).hasMom !== undefined;
 }
 
