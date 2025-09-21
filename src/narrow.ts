8662b468f7ee6a61153c005914032942bf54aeb2
BumTiger <jbpark03@gmail.com>
2025-09-21 12:42:24 +0900

    tell if cat, call the teller function

--- src/narrow.ts
+++ src/narrow.ts
@@ -5,3 +5,17 @@ class CatType {
 class DogType {
   constructor(public regiNumber: number){};
 }
+
+const makeSound = (pet : CatType | DogType) => {
+  if (isCat(pet)) {
+    return `야옹. 엄마 있다:`;
+  } else {
+    return "멍멍";
+  }
+}
+
+function isCat(pet : CatType | DogType){
+  return (pet as CatType).hasMom !== undefined;
+}
+
+console.log(makeSound({hasMom: true}));
